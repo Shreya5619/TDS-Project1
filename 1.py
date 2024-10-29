052dbb9a58ff0a84d0656086605035c016ab6157
Shreya Prasad <shreyaprasad019@gmail.com>
2024-10-29 23:21:07 +0530

    Added CSVs

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import csv
 
-# Define the list to store users from Delhi
+# Define the list to store users from Austin
 users_in_austin = []
 
 # Read the CSV file with UTF-8 encoding
@@ -8,7 +8,7 @@ with open('users.csv', 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
         location = row['location'].strip().lower()
-        # Check if the user is from Delhi
+        # Check if the user is from Austin
         if 'austin' in location:
             users_in_austin.append({
                 'login': row['login'],
